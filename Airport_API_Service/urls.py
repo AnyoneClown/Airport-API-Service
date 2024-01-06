17c8fc15661052cfe7fca2d58fc1360f723ccd6b
Denis Dymerlii <dum.denya15@gmail.com>
2024-01-06 13:20:36 +0200

    register urls for airport app

--- Airport_API_Service/urls.py
+++ Airport_API_Service/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/airport/", include("airport.urls", namespace="airport")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
