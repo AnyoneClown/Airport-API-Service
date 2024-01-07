0d8df6518b2df09e13347a65efb03a838a44dbe0
Denis Dymerlii <dum.denya15@gmail.com>
2024-01-07 22:50:47 +0200

    add throttle rates

--- Airport_API_Service/settings.py
+++ Airport_API_Service/settings.py
@@ -139,7 +139,15 @@ AUTH_USER_MODEL = "user.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    }
 }
 
 SIMPLE_JWT = {
